a9b997d8f38e620fac422a39562c05b2602a6080
O.Krasovskyi <mechanosec29@gmail.com>
2025-08-24 00:11:59 +0300

    init

---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- config = function()
+    --   ---@diagnostic disable-next-line: missing-fields
+    --   require('tokyonight').setup {
+    --     styles = {
+    --       comments = { italic = false }, -- Disable italics in comments
+    --     },
+    --   }
+    --   vim.cmd.colorscheme 'tokyonight-night'
+    -- end,
+  },
+}
