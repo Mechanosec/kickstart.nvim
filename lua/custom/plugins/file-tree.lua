3599f166a43e8d1d4080ce47e54ee10dce33c96a
O.Krasovskyi <mechanosec29@gmail.com>
2025-08-28 17:27:28 +0300

    update tree

---
+++ lua/custom/plugins/file-tree.lua
@@ -0,0 +1,40 @@
+return {
+  -- {
+  --   'simonmclean/triptych.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim', -- required
+  --     'nvim-tree/nvim-web-devicons', -- optional for icons
+  --     'antosha417/nvim-lsp-file-operations', -- optional LSP integration
+  --   },
+  --   opts = {}, -- config options here
+  --   keys = {
+  --     { '<leader>e', ':Triptych<CR>', desc = '[T]ree' },
+  --   },
+  -- },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      {
+        'antosha417/nvim-lsp-file-operations', -- optional LSP integration
+        opts = {},
+      },
+    },
+    lazy = false,
+    keys = {
+      { '<leader>e', ':Neotree reveal<CR>', desc = '[T]ree', silent = true },
+    },
+    opts = {
+      -- filesystem = {
+      --   window = {
+      --     mappings = {
+      --       ['\\'] = 'close_window',
+      --     },
+      --   },
+      -- },
+    },
+  },
+}
